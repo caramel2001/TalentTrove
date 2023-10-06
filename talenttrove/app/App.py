76da3461f19ca64b4d5081047884d93af37fe1cf
Pratham <agarwalpratham2001@gmail.com>
2023-10-07 00:22:59 +0800

    Added Streamlit skeleton

---
+++ talenttrove/app/App.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def intro():
+    import streamlit as st
+
+    st.write("# Welcome to TalentTrove! 👋")
+    # st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        TalentTrove is an open-source app framework built specifically for
+        Tracking and managing your Job Applications.
+
+        
+    """
+    )
+intro()
