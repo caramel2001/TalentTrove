349bf96554ff3f0f9d0540ddf0a3e5d270ff9023
#AGARWALA PRATHAM# <pratham001@SCSEGPU-TC1.cm.cluster>
2023-11-19 23:31:14 +0800

    Added Job UI

--- talenttrove/app/pages/2_Matches.py
+++ talenttrove/app/pages/2_Matches.py
@@ -4,13 +4,20 @@ openai_api_key = st.session_state.get("OPENAI_API_KEY")
 gmail_api_key = st.session_state.get("GMAIL_API_KEY")
 
 
+
+
+uploaded_file = st.file_uploader(
+    "Upload your Resumse (Supported pdf, docx)",
+    type=["pdf", "docx"],
+    help="Scanned documents are not supported yet!",
+)
+if not uploaded_file:
+    st.warning(
+        "Upload your Resumse (Supported pdf, docx) to get LLM recommended Matches"
+    )
+
 if not openai_api_key:
     st.warning(
         "Enter your OpenAI API key in the sidebar. You can get a key at"
-        " https://platform.openai.com/account/api-keys."
-    )
-if not gmail_api_key:
-    st.warning(
-        "Enter your Gmail API key in the sidebar. You can get a key at"
         " https://platform.openai.com/account/api-keys."
     )
